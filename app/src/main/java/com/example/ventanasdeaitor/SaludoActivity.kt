bebb3e5cf95b6bbf4cc89063e3addc6ac37ba63f
ZuberoClase <azuberogondam1b@gmail.com>
2024-01-29 16:17:34 +0100

    primer commit

---
+++ app/src/main/java/com/example/ventanasdeaitor/SaludoActivity.kt
@@ -0,0 +1,24 @@
+package com.example.ventanasdeaitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SaludoActivity : AppCompatActivity() {
+
+
+    private lateinit var txtSaludo: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo)
+
+
+        txtSaludo = findViewById(R.id.txtSaludo)
+
+        val bundle = intent.extras
+
+        txtSaludo.text = "Hola ${bundle?.getStringArrayList("NOMBRE")}"
+
+
+    }
+}
